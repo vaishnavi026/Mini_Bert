d3f24e2b49f62435f3f639afd5cf1f9bb2605875
vaishnavi026 <f2015777@pilani.bits-pilani.ac.in>
2023-10-09 13:55:18 -0500

    Modified runeexp

--- assignment2/run_exp.sh
+++ assignment2/run_exp.sh
@@ -8,7 +8,7 @@ mkdir -p $CAMPUSID
 # Step 2. Train models on two datasets.
 ##  2.1. Run experiments on SST
 PREF='sst'
-python classifier.py \
+python3 classifier.py \
     --use_gpu \
     --option finetune \
     --lr 1e-5 \
@@ -18,11 +18,12 @@ python classifier.py \
     --test "data/${PREF}-test.txt" \
     --dev_out "${CAMPUSID}/${PREF}-dev-output.txt" \
     --test_out "${CAMPUSID}/${PREF}-test-output.txt" \
-    --filepath "${CAMPUSID}/${PREF}-model.pt" | tee ${CAMPUSID}/${PREF}-train-log.txt
+    --filepath "${CAMPUSID}/${PREF}-model.pt"
+#    | tee ${CAMPUSID}/${PREF}-train-log.txt
 
 ##  2.2 Run experiments on CF-IMDB
 PREF='cfimdb'
-python classifier.py \
+python3 classifier.py \
     --use_gpu \
     --option finetune \
     --lr 1e-5 \
@@ -32,17 +33,18 @@ python classifier.py \
     --test "data/${PREF}-test.txt" \
     --dev_out "${CAMPUSID}/${PREF}-dev-output.txt" \
     --test_out "${CAMPUSID}/${PREF}-test-output.txt" \
-    --filepath "${CAMPUSID}/${PREF}-model.pt" | tee ${CAMPUSID}/${PREF}-train-log.txt
+    --filepath "${CAMPUSID}/${PREF}-model.pt"
+#    | tee ${CAMPUSID}/${PREF}-train-log.txt
 
 
 
 # Step 3. Prepare submission:
 ##  3.1. Copy your code to the $CAMPUSID folder
-for file in *.py; do cp $file ${CAMPUSID}/; done
-for file in *.sh; do cp $file ${CAMPUSID}/; done
-for file in *.md; do cp $file ${CAMPUSID}/; done
-for file in *.txt; do cp $file ${CAMPUSID}/; done
+#for file in *.py; do cp $file ${CAMPUSID}/; done
+#for file in *.sh; do cp $file ${CAMPUSID}/; done
+#for file in *.md; do cp $file ${CAMPUSID}/; done
+#for file in *.txt; do cp $file ${CAMPUSID}/; done
 
 ##  3.2. Compress the $CAMPUSID folder to $CAMPUSID.zip (containing only .py/.txt/.pdf/.sh files)
-python prepare_submit.py ${CAMPUSID} ${CAMPUSID}
+#python prepare_submit.py ${CAMPUSID} ${CAMPUSID}
 ##  3.3. Submit the zip file to Canvas! Congrats!
