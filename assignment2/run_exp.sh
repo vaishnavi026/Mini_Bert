46d4a903656c23abceb4bef3ad998c7c6280912f
vaishnavi026 <f2015777@pilani.bits-pilani.ac.in>
2023-10-09 15:00:24 -0500

    run_exp

--- assignment2/run_exp.sh
+++ assignment2/run_exp.sh
@@ -18,8 +18,7 @@ python3 classifier.py \
     --test "data/${PREF}-test.txt" \
     --dev_out "${CAMPUSID}/${PREF}-dev-output.txt" \
     --test_out "${CAMPUSID}/${PREF}-test-output.txt" \
-    --filepath "${CAMPUSID}/${PREF}-model.pt"
-#    | tee ${CAMPUSID}/${PREF}-train-log.txt
+    --filepath "${CAMPUSID}/${PREF}-model.pt" | tee ${CAMPUSID}/${PREF}-train-log.txt
 
 ##  2.2 Run experiments on CF-IMDB
 PREF='cfimdb'
@@ -33,18 +32,17 @@ python3 classifier.py \
     --test "data/${PREF}-test.txt" \
     --dev_out "${CAMPUSID}/${PREF}-dev-output.txt" \
     --test_out "${CAMPUSID}/${PREF}-test-output.txt" \
-    --filepath "${CAMPUSID}/${PREF}-model.pt"
-#    | tee ${CAMPUSID}/${PREF}-train-log.txt
+    --filepath "${CAMPUSID}/${PREF}-model.pt" | tee ${CAMPUSID}/${PREF}-train-log.txt
 
 
 
 # Step 3. Prepare submission:
 ##  3.1. Copy your code to the $CAMPUSID folder
-#for file in *.py; do cp $file ${CAMPUSID}/; done
-#for file in *.sh; do cp $file ${CAMPUSID}/; done
-#for file in *.md; do cp $file ${CAMPUSID}/; done
-#for file in *.txt; do cp $file ${CAMPUSID}/; done
+for file in *.py; do cp $file ${CAMPUSID}/; done
+for file in *.sh; do cp $file ${CAMPUSID}/; done
+for file in *.md; do cp $file ${CAMPUSID}/; done
+for file in *.txt; do cp $file ${CAMPUSID}/; done
 
 ##  3.2. Compress the $CAMPUSID folder to $CAMPUSID.zip (containing only .py/.txt/.pdf/.sh files)
-#python prepare_submit.py ${CAMPUSID} ${CAMPUSID}
-##  3.3. Submit the zip file to Canvas! Congrats!
+python3 prepare_submit.py ${CAMPUSID} ${CAMPUSID}
+#  3.3. Submit the zip file to Canvas! Congrats!
